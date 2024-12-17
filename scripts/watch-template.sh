ccdcf87d8f07c341ab28fce1506c2c59b3306ed9
Diego Tenjo <tenjodiego961@gmail.com>
2024-12-17 13:59:53 -0500

    Add script watcher (.template) for create atomatically files

---
+++ scripts/watch-template.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Directory for dotfiles
+WATCH_DIR="$PATH_DOTFILES"
+
+# Use inotifywait to watch for changes in the directory and subdirectories
+inotifywait -m -r -e close_write --format "%w%f" "$WATCH_DIR" | while read file; do
+    # Check if the file has a .template extension
+    if [[ "$file" =~ \.template$ ]]; then
+        # Generate the output file name without the .template extension
+        output_file="${file%.template}"
+        echo "Generating $output_file from $file"
+
+        # Run envsubst to substitute environment variables and create the file
+        envsubst < "$file" > "$output_file"
+    fi
+done
